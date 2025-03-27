57ddabdc3cb38e54fca5eb9d94d0618429e5fbe9
calebe <rcalebe655@gmail.com>
2025-03-27 18:37:59 -0300

    Workplace update

--- Workplace/myproject/myapp/urls.py
+++ Workplace/myproject/myapp/urls.py
@@ -1,6 +1,6 @@
 from django.urls import path
-from . import views
+from myapp import views
 
 urlpatterns = [
-    path('', views.home),
+    path('dishes/<str:dish>', views.menuitems),
 ]
--- Workplace/myproject/myapp/views.py
+++ Workplace/myproject/myapp/views.py
@@ -2,5 +2,12 @@ from django.shortcuts import render
 from django.http import HttpResponse
 # Create your views here.
 
-def home(request):
-    return HttpResponse("Welcome to Little Lemon restaurant!")
+def menuitems(request, dish):
+    items = {
+        'pasta': 'Pasta is a type of noodle made from combination of wheat, water or eggs',
+        'falafel': 'Falafel are deep fried patties',
+        'cheesecake': 'Cheesecake is a type of dessert made with cream, soft cheese on top of cookie, pastry crust or graham cracker and fruit sauce topping',
+    }
+
+    description = items[dish]
+    return HttpResponse(f"<h2> {dish} </h2>" + description)
