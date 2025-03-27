d14a2c59776491e66aa6e789ed4be23e40d9bffe
calebe <rcalebe655@gmail.com>
2025-03-27 13:52:49 -0300

    criate function index

--- Main/handlingdata/myapp/views.py
+++ Main/handlingdata/myapp/views.py
@@ -15,3 +15,13 @@ def display_date(request):
 def menu(request):
     text = """<h1 style="color: #F4CE14;"> This is Little Lemon again!</h1>"""
     return HttpResponse(text)
+
+def index(request): 
+    path = request.path 
+    method = request.method 
+    content=''' 
+<center><h2>Testing Django Request Response Objects</h2> 
+<p>Request path : " {}</p> 
+<p>Request Method :{}</p></center> 
+'''.format(path, method) 
+    return HttpResponse(content) 
