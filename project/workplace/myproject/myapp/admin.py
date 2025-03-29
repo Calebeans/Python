b462c788833ce1cd2ea1286ae4c68d7e0f8034f5
calebe <rcalebe655@gmail.com>
2025-03-29 19:18:45 -0300

    Update code

---
+++ project/workplace/myproject/myapp/admin.py
@@ -0,0 +1,10 @@
+from django.contrib import admin
+from .models import Drinks
+from .models import DrinksCategory
+
+# Register your models here.
+admin.site.register(Drinks)
+admin.site.register(DrinksCategory)
+# Import the models from models.py
+
+# Register your models here.
